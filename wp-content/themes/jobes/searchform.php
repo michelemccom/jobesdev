66f26d8ea2ec6b62ed4825dfe56df010b8f6c3aa
michelemccom <michele_martin@mccom.com>
2016-02-09 09:15:30 -0600

    header

--- wp-content/themes/jobes/searchform.php
+++ wp-content/themes/jobes/searchform.php
@@ -1,4 +1,4 @@
 <form role="search" method="get" id="site_search" action="<?php echo get_option('home'); ?>/" >
-    <input type="text" name="s" id="s" class="text" placeholder="Search" value="" />
+    <input type="text" name="s" id="s" class="text" placeholder="I'm looking for..." value="" />
     <button id="search_submit" type="submit">Go</button>
 </form>
