e81d016d8ecc3c8f16dcc1df57dba900365bb61e
michelemccom <michele_martin@mccom.com>
2016-02-09 10:20:47 -0600

    background image

--- wp-content/themes/jobes/header.php
+++ wp-content/themes/jobes/header.php
@@ -34,7 +34,7 @@
         $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) ); ?> 
         style="background-image: url('<?php echo $image[0]; ?>')" 
     <?php } else { ?> 
-        style="background: url(image/jobes-bg/png);" } ?>>
+        style="background: url(image/jobes_bg.png);" } ?>>
 
 <div id="page">
 
