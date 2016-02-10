0ab92de9ca10cec86809e2f27dae35dcbde33bc1
michelemccom <michele_martin@mccom.com>
2016-02-10 10:41:24 -0600

    page styles

--- wp-content/themes/jobes/page.php
+++ wp-content/themes/jobes/page.php
@@ -9,7 +9,7 @@ get_header(); ?>
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<div class="post" id="post-<?php the_ID(); ?>">
-		<h1><?php the_title(); ?></h1>
+		<h1 class="page-title"><?php the_title(); ?></h1>
 		<?php the_content(); ?>
 		
 		<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
