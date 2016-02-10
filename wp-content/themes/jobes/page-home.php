282396d2cc56d488e4bde693dd537dbb4c0bdb84
michelemccom <michele_martin@mccom.com>
2016-02-10 16:00:55 -0600

    blocks

--- wp-content/themes/jobes/page-home.php
+++ wp-content/themes/jobes/page-home.php
@@ -17,7 +17,7 @@ get_header(); ?>
 			<?php $attachment_id = $post->ID;
 			$image_attributes = wp_get_attachment_image_src( $attachment_id ); ?>
 			<div class="round">
-				<img src="<?php echo $image_attributes[0]?>" width="<?php echo $image_attributes[1]?>" height="'<?php echo $image_attributes[2]?>">';	
+				<img src="<?php echo $image_attributes[0];?>" width="<?php echo $image_attributes[1];?>" height="'<?php echo $image_attributes[2]; ?>">';	
 			</div>
 			<a href="<?php echo get_post_meta($post->ID, "block_one_link", true);?>"><?php echo get_post_meta($post->ID, "block_one_text", true);?></a>
 		</div>
