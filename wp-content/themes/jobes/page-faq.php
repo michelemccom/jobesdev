cc669ea703b32916526d2da000cae835019b0b34
michelemccom <michele_martin@mccom.com>
2016-02-10 14:53:24 -0600

    faq

--- wp-content/themes/jobes/page-faq.php
+++ wp-content/themes/jobes/page-faq.php
@@ -12,7 +12,7 @@ get_header(); ?>
 	<div class="post" id="post-<?php the_ID(); ?>">
 		<h1 class="page-title"><?php the_title(); ?></h1>
 		<?php the_content(); ?>
-		
+		<ul>
 		<?php 
 	    $args = array(
 	        'post_type' => 'faq',
@@ -22,9 +22,9 @@ get_header(); ?>
 	        );
 	    $the_query = new WP_Query($args);
 	    if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post(); ?>
-	        <a href="<?php the_permalink(); ?>"><?php the_title();?><a href="<?php the_permalink(); ?>">
+	       <li> <a href="<?php the_permalink(); ?>"><?php the_title();?><a href="<?php the_permalink(); ?>"></li>
 	    <?php endwhile; endif;wp_reset_postdata();?>
-		
+		</ul>	
 	</div>
 	<?php endwhile; endif; ?>   
 </div> <!-- end copy -->
