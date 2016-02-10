dca0f2778b4a1f6f445e314f98b50fa1203fb453
michelemccom <michele_martin@mccom.com>
2016-02-10 16:28:50 -0600

    blocks

--- wp-content/themes/jobes/page-home.php
+++ wp-content/themes/jobes/page-home.php
@@ -15,13 +15,27 @@ get_header(); ?>
 	<div class="blocks">
 		<div class="block one">
 			<?php $attachment_id = get_post_meta( $post->ID, 'block_one_image', true ); 
-			$image_attributes = wp_get_attachment_image_src( $attachment_id );
-			get_post_meta( $post->ID, 'block_one_image', true ); 
-			var_dump($image_attributes);?>
+			$image_attributes_one = wp_get_attachment_image_src( $attachment_id ); ?>
 			<div class="round">
-				<img src="<?php echo $image_attributes[0];?>" width="<?php echo $image_attributes[1];?>" height="'<?php echo $image_attributes[2]; ?>">';	
+				<img src="<?php echo $image_attributes_one[0];?>" width="<?php echo $image_attributes_one[1];?>" height="'<?php echo $image_attributes_one[2]; ?>">';	
 			</div>
 			<a href="<?php echo get_post_meta($post->ID, "block_one_link", true);?>"><?php echo get_post_meta($post->ID, "block_one_text", true);?></a>
+		</div>
+		<div class="block two">
+			<?php $attachment_id = get_post_meta( $post->ID, 'block_two_image', true ); 
+			$image_attributes_two = wp_get_attachment_image_src( $attachment_id ); ?>
+			<div class="round">
+				<img src="<?php echo $image_attributes_two[0];?>" width="<?php echo $image_attributes_two[1];?>" height="'<?php echo $image_attributes_two[2]; ?>">';	
+			</div>
+			<a href="<?php echo get_post_meta($post->ID, "block_two_link", true);?>"><?php echo get_post_meta($post->ID, "block_two_text", true);?></a>
+		</div>
+		<div class="block three">
+			<?php $attachment_id = get_post_meta( $post->ID, 'block_three_image', true ); 
+			$image_attributes_three = wp_get_attachment_image_src( $attachment_id ); ?>
+			<div class="round">
+				<img src="<?php echo $image_attributes_three[0];?>" width="<?php echo $image_attributes_three[1];?>" height="'<?php echo $image_attributes_three[2]; ?>">';	
+			</div>
+			<a href="<?php echo get_post_meta($post->ID, "block_three_link", true);?>"><?php echo get_post_meta($post->ID, "block_three_text", true);?></a>
 		</div>
 	</div>
 </div> <!-- end copy -->
