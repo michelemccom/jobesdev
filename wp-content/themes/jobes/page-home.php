f484565671e8e95461f47326a375804dc6d8770e
michelemccom <michele_martin@mccom.com>
2016-02-10 15:58:05 -0600

    home blocks

--- wp-content/themes/jobes/page-home.php
+++ wp-content/themes/jobes/page-home.php
@@ -10,9 +10,18 @@ get_header(); ?>
 	<div class="banner">
 		<?php echo get_post_meta($post->ID, "home_banner_message", true);?>
 	</div>
-
 <div id="copy"> 
 	<?php the_content(); ?>
+	<div class="blocks">
+		<div class="block one">
+			<?php $attachment_id = $post->ID;
+			$image_attributes = wp_get_attachment_image_src( $attachment_id ); ?>
+			<div class="round">
+				<img src="<?php echo $image_attributes[0]?>" width="<?php echo $image_attributes[1]?>" height="'<?php echo $image_attributes[2]?>">';	
+			</div>
+			<a href="<?php echo get_post_meta($post->ID, "block_one_link", true);?>"><?php echo get_post_meta($post->ID, "block_one_text", true);?></a>
+		</div>
+	</div>
 </div> <!-- end copy -->
 <?php endwhile; endif; ?>
 <?php get_footer(); ?>
