a497929eff1366dffcdd3bff8b3aae9efcdde2b9
michelemccom <michele_martin@mccom.com>
2016-02-11 16:37:33 -0600

    blocks

--- wp-content/themes/jobes/page-home.php
+++ wp-content/themes/jobes/page-home.php
@@ -17,7 +17,7 @@ get_header(); ?>
 			<?php $attachment_id = get_post_meta( $post->ID, 'block_one_image','block', true ); 
 			$image_attributes_one = wp_get_attachment_image_src( $attachment_id ); ?>
 			<div class="round">
-				<img src="<?php echo $image_attributes_one[0];?>" width="<?php echo $image_attributes_one[1];?>" height="'<?php echo $image_attributes_one[2]; ?>">';	
+				<img src="<?php echo $image_attributes_one[0];?>" width="<?php echo $image_attributes_one[1];?>" height="<?php echo $image_attributes_one[2]; ?>">	
 			</div>
 			<a href="<?php echo get_post_meta($post->ID, "block_one_link", true);?>"><?php echo get_post_meta($post->ID, "block_one_text", true);?></a>
 		</div>
@@ -25,7 +25,7 @@ get_header(); ?>
 			<?php $attachment_id = get_post_meta( $post->ID, 'block_two_image', 'block', true ); 
 			$image_attributes_two = wp_get_attachment_image_src( $attachment_id ); ?>
 			<div class="round">
-				<img src="<?php echo $image_attributes_two[0];?>" width="<?php echo $image_attributes_two[1];?>" height="'<?php echo $image_attributes_two[2]; ?>">';	
+				<img src="<?php echo $image_attributes_two[0];?>" width="<?php echo $image_attributes_two[1];?>" height="'<?php echo $image_attributes_two[2]; ?>">
 			</div>
 			<a href="<?php echo get_post_meta($post->ID, "block_two_link", true);?>"><?php echo get_post_meta($post->ID, "block_two_text", true);?></a>
 		</div>
@@ -33,7 +33,7 @@ get_header(); ?>
 			<?php $attachment_id = get_post_meta( $post->ID, 'block_three_image', 'block', true ); 
 			$image_attributes_three = wp_get_attachment_image_src( $attachment_id ); ?>
 			<div class="round">
-				<img src="<?php echo $image_attributes_three[0];?>" width="<?php echo $image_attributes_three[1];?>" height="'<?php echo $image_attributes_three[2]; ?>">';	
+				<img src="<?php echo $image_attributes_three[0];?>" width="<?php echo $image_attributes_three[1];?>" height="'<?php echo $image_attributes_three[2]; ?>">
 			</div>
 			<a href="<?php echo get_post_meta($post->ID, "block_three_link", true);?>"><?php echo get_post_meta($post->ID, "block_three_text", true);?></a>
 		</div>
