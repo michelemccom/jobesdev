b82a6576423b65f3d76c566aad9f71777edf53e8
michelemccom <michele_martin@mccom.com>
2016-02-11 16:30:57 -0600

    home blocks

--- wp-content/themes/jobes/page-home.php
+++ wp-content/themes/jobes/page-home.php
@@ -14,7 +14,7 @@ get_header(); ?>
 	<?php the_content(); ?>
 	<div class="blocks">
 		<div class="block one">
-			<?php $attachment_id = get_post_meta( $post->ID, 'block_one_image', true ); 
+			<?php $attachment_id = get_post_meta( $post->ID, 'block_one_image','block', true ); 
 			$image_attributes_one = wp_get_attachment_image_src( $attachment_id ); ?>
 			<div class="round">
 				<img src="<?php echo $image_attributes_one[0];?>" width="<?php echo $image_attributes_one[1];?>" height="'<?php echo $image_attributes_one[2]; ?>">';	
@@ -22,7 +22,7 @@ get_header(); ?>
 			<a href="<?php echo get_post_meta($post->ID, "block_one_link", true);?>"><?php echo get_post_meta($post->ID, "block_one_text", true);?></a>
 		</div>
 		<div class="block two">
-			<?php $attachment_id = get_post_meta( $post->ID, 'block_two_image', true ); 
+			<?php $attachment_id = get_post_meta( $post->ID, 'block_two_image', 'block', true ); 
 			$image_attributes_two = wp_get_attachment_image_src( $attachment_id ); ?>
 			<div class="round">
 				<img src="<?php echo $image_attributes_two[0];?>" width="<?php echo $image_attributes_two[1];?>" height="'<?php echo $image_attributes_two[2]; ?>">';	
@@ -30,7 +30,7 @@ get_header(); ?>
 			<a href="<?php echo get_post_meta($post->ID, "block_two_link", true);?>"><?php echo get_post_meta($post->ID, "block_two_text", true);?></a>
 		</div>
 		<div class="block three">
-			<?php $attachment_id = get_post_meta( $post->ID, 'block_three_image', true ); 
+			<?php $attachment_id = get_post_meta( $post->ID, 'block_three_image', 'block', true ); 
 			$image_attributes_three = wp_get_attachment_image_src( $attachment_id ); ?>
 			<div class="round">
 				<img src="<?php echo $image_attributes_three[0];?>" width="<?php echo $image_attributes_three[1];?>" height="'<?php echo $image_attributes_three[2]; ?>">';	
