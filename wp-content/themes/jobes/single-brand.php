d1676d695429866912c669d4ec5db3c45796919f
michelemccom <michele_martin@mccom.com>
2016-02-16 14:22:12 -0600

    brands

--- wp-content/themes/jobes/single-brand.php
+++ wp-content/themes/jobes/single-brand.php
@@ -14,6 +14,36 @@ get_header();
         <h1 class="page-title"><?php the_title(); ?></h1>
         
             <?php  the_content();
+
+            $doctors = get_posts(array(
+							'post_type' => 'brand',
+							'meta_query' => array(
+								array(
+									'key' => 'product', // name of custom field
+									'value' => '"' . get_the_ID() . '"', // matches exaclty "123", not just 123. This prevents a match for "1234"
+									'compare' => 'LIKE'
+								)
+							)
+						));
+
+						?>
+						<?php if( $doctors ): ?>
+							<ul>
+							<?php foreach( $doctors as $doctor ): ?>
+								<?php 
+
+								$photo = get_field('photo', $doctor->ID);
+
+								?>
+								<li>
+									<a href="<?php echo get_permalink( $doctor->ID ); ?>">
+								
+										<?php echo get_the_title( $doctor->ID ); ?>
+									</a>
+								</li>
+							<?php endforeach; ?>
+							</ul>
+						<?php endif; 
   		$brands = get_field('brand');
 
 						?>
