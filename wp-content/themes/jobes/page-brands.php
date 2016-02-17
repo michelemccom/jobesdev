27d59c6c1431462ca9152e2fa0a4e1b91f46c19e
michelemccom <michele_martin@mccom.com>
2016-02-17 16:29:14 -0600

    brands

---
+++ wp-content/themes/jobes/page-brands.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Jobes
+ * Template Name: Brands
+ */
+
+get_header(); ?>
+<div id="copy"> 
+
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<div class="post" id="post-<?php the_ID(); ?>">
+		<h1 class="page-title"><?php the_title(); ?></h1>
+		<?php the_content(); ?>
+		
+		<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+	</div>
+	<?php endwhile; endif; ?>   
+</div> <!-- end copy -->
+
+<?php get_footer(); ?>
