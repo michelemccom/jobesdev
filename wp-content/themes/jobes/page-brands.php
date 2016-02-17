d1c64b8e6d4dd72d97ed8dd7f8b7ada583796cde
michelemccom <michele_martin@mccom.com>
2016-02-17 16:39:18 -0600

    brands

--- wp-content/themes/jobes/page-brands.php
+++ wp-content/themes/jobes/page-brands.php
@@ -12,20 +12,24 @@ get_header(); ?>
 	<div class="post" id="post-<?php the_ID(); ?>">
 		<h1 class="page-title"><?php the_title(); ?></h1>
 		<?php the_content(); 
-		
-$field = get_post_meta($post->ID, "product_categories", true);
-$value = $field['value'];
-$choices = $field['choices'];
 
-if( $value ): ?>
-<ul>
-	<?php foreach( $value as $v ): ?>
-	<li>
-		<?php echo $choices[ $v ]; ?>
-	</li>
-	<?php endforeach; ?>
-</ul>
+
+$posts = get_post_meta($post->ID, "product_categories", true);
+
+if( $posts ): ?>
+    <ul>
+    <?php foreach( $posts as $post): // variable must be called $post (IMPORTANT) ?>
+        <?php setup_postdata($post); ?>
+        <li>
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            <span>Custom field from $post: <?php the_field('author'); ?></span>
+        </li>
+    <?php endforeach; ?>
+    </ul>
+    <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
 <?php endif; ?>
+
+
 		<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
 	</div>
 	<?php endwhile; endif; ?>   
