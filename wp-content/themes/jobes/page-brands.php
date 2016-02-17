ddfa26cba66366fed31d69984f88570988df722f
michelemccom <michele_martin@mccom.com>
2016-02-17 16:35:37 -0600

    brands

--- wp-content/themes/jobes/page-brands.php
+++ wp-content/themes/jobes/page-brands.php
@@ -11,8 +11,21 @@ get_header(); ?>
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<div class="post" id="post-<?php the_ID(); ?>">
 		<h1 class="page-title"><?php the_title(); ?></h1>
-		<?php the_content(); ?>
+		<?php the_content(); 
 		
+$field = get_post_meta($post->ID, "product_categories", true);
+$value = $field['value'];
+$choices = $field['choices'];
+
+if( $value ): ?>
+<ul>
+	<?php foreach( $value as $v ): ?>
+	<li>
+		<?php echo $choices[ $v ]; ?>
+	</li>
+	<?php endforeach; ?>
+</ul>
+<?php endif; ?>
 		<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
 	</div>
 	<?php endwhile; endif; ?>   
