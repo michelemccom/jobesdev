afdcafda47410ef336aae78ed31ff357b2965419
michelemccom <michele_martin@mccom.com>
2016-02-18 11:41:43 -0600

    categories

--- wp-content/themes/jobes/taxonomy-product_brands.php
+++ wp-content/themes/jobes/taxonomy-product_brands.php
@@ -34,13 +34,26 @@ get_header();
             )
           ); 
           $the_query = new WP_Query($args);
-        if ($the_query->have_posts()) : ?>
-        <ul>
-        <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-           <li> <a href="<?php the_permalink(); ?>"><?php the_title();?><a href="<?php the_permalink(); ?>"></li>
-        <?php endwhile; ?>
-         </ul>  
-        <?php endif;wp_reset_postdata();?>
+        if ($the_query->have_posts()) :  while ($the_query->have_posts()) : $the_query->the_post(); 
+        $args=array(
+            'hide_empty'        => 0,
+            'parent'        => 0,
+            'taxonomy'      => 'product_categories');
+        $categories=get_categories($args);
+         
+            foreach($categories as $category) {
+                echo '<div class="product-cat">'; 
+             
+                $thumb_url = get_option('taxonomy_image_plugin');
+                $product_cat_url = get_term_link( $category->slug, 'product_categories' );
+                             
+                echo '<a href="'.$product_cat_url.'"><img src="'.get_template_directory_uri().'/timthumb.php?src='.wp_get_attachment_url( $thumb_url[$category->term_taxonomy_id]).'&h=200&w=200" /></a>';
+                echo '<a class="cat-title" href="' . $product_cat_url .'">' . $category->name.'</a>';
+                echo '</div> <!--end product cat-->';
+            }?>
+
+    
+        <?php endwhile; endif;wp_reset_postdata();?>
      
         
 
