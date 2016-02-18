56e49f99a4d8f126ea05537954b9e34c7ef3fea1
michelemccom <michele_martin@mccom.com>
2016-02-18 15:07:51 -0600

    brands

--- wp-content/themes/jobes/taxonomy-brands.php
+++ wp-content/themes/jobes/taxonomy-brands.php
@@ -36,7 +36,6 @@ get_header();
 
         
           ); 
-             );
         $the_query = new WP_Query($args);
         if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post(); ?>
         <li><?php the_title();?></li>
