ed7f8870cfd96d8b0c5971b04f0c2c0ebba40655
michelemccom <michele_martin@mccom.com>
2016-02-18 11:28:28 -0600

    brands

--- wp-content/themes/jobes/taxonomy-product_brands.php
+++ wp-content/themes/jobes/taxonomy-product_brands.php
@@ -9,15 +9,12 @@ get_header();
 
 <div id="copy"> 
 <?php 
-  $slug_products = get_query_var( 'term' );
-  $term_products = get_term_by( 'slug', $slug_products, 'product_categories' );
-  $term_id_products = $term_products->term_id;
 
   $slug_brands = get_query_var( 'term' );
   $term_brands = get_term_by( 'slug', $slug_brands, 'product_brands' );
   $term_id_brands = $term_brands->term_id;
 ?>
-
+        <h1></h1>
         <p><?php echo $term_brands->description; ?></p>
 
         <?php
@@ -25,15 +22,7 @@ get_header();
             'post_type' => 'products',
 
             'tax_query' => array(
-              'relation' => 'AND',
 
-              array(
-                'taxonomy'      => 'product_categories',
-                'hide_empty'    => 0,
-                'parent'        => $term_id_products,
-                'terms'         => $term_products,
-                'field'         => 'slug',
-              ),
 
               array(
                 'taxonomy'      => 'product_brands',
@@ -43,90 +32,17 @@ get_header();
                 'field'         => 'slug',
               )
             )
-          );
-
-          $categories=get_categories($args);
-
-          if(!$categories){ 
-
-          //get the product category name
-          echo "<h3>' .$term_brands->name. ' Products</h3>";
-
-          $args = array(
-            'posts_per_page' => 50, //remember posts per page should be less or more that what's set in general settings
-            'paged' => $paged,
-            'order' => 'ASC',
-            'tax_query' => array(
-              'relation' => 'AND',
-
-              array(
-                'taxonomy'      => 'product_categories',
-                'hide_empty'    => 0,
-                'parent'        => $term_id_products,
-                'terms'         => $term_products,
-                'field'         => 'slug',
-              ),
-
-              array(
-                'taxonomy'      => 'product_brands',
-                'hide_empty'    => 0,
-                'parent'        => $term_id_brands,
-                'terms'         => $term_brands,
-                'field'         => 'slug',
-              )
-            )
-          );
-        ?>
-
-        <!-- if there are no subcategories output current product category's products -->
-        <?php
-          $products_query = new WP_Query($args);
-          if (have_posts()) : while($products_query->have_posts()) : $products_query->the_post(); 
-
-          $thumb_url = get_option('taxonomy_image_plugin');
-        ?>
-
-        <div class="large-4 column product-cat end" onclick="location.href='<?php echo get_permalink(); ?>';">
-          <div class="panel" data-equalizer-watch>
-            <div class="prod-img">
-
-              <?php
-                if ( has_post_thumbnail( $post->ID ) ) :
-                  $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
-                  $image = $image[0];
-                  echo '<img src=" ' . $image . ' " />';
-                endif;
-              ?>
-
-            </div>
-          </div>
-
-          <a class="cat-title" href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
-        </div>
-
-        <?php endwhile; endif; ?>
-
-        <?php } else { 
-
-          //output current category name - MOST OF THE ACTION IS HERE
-          echo '<h3>'.$term_brands->name.' Products</h3>';
-
-          foreach($categories as $category) {
-            $product_cat_url = get_term_link( $category->slug, 'product_categories' );
-            // $product_cat_url = get_term_link( $term_products, 'product_categories' );
-
-            $thumb_url = get_option('taxonomy_image_plugin');
-        ?>
-
-        <div class="large-4 medium-4 column product-cat" onclick="location.href='<?php echo $product_cat_url ; ?>';">
-          <div class="panel" data-equalizer-watch>
-            <div class="prod-img"><img src="<?php echo wp_get_attachment_url( $thumb_url[$category->term_id]); ?>" /></div>
-          </div>
-
-          <a class="cat-title" href="<?php echo $product_cat_url ; ?>"><?php echo $category->name; ?></a>
-        </div>
-
-      <?php } } ?>
+          ); 
+          $the_query = new WP_Query($args);
+        if ($the_query->have_posts()) : ?>
+        <ul>
+        <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+           <li> <a href="<?php the_permalink(); ?>"><?php the_title();?><a href="<?php the_permalink(); ?>"></li>
+        <?php endwhile; ?>
+         </ul>  
+        <?php endif;wp_reset_postdata();?>
+     
+        
 
       </div> <!--end of entry-->      
 
