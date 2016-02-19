85b308ea758d0d2c2a22d3c23e69ae56ce8e46d6
michelemccom <michele_martin@mccom.com>
2016-02-19 10:06:04 -0600

    terms

--- wp-content/themes/jobes/taxonomy-brands.php
+++ wp-content/themes/jobes/taxonomy-brands.php
@@ -9,12 +9,13 @@ get_header();
 
 <div id="copy"> 
 <?php 
+
   $slug_brands = get_query_var( 'term' );
   $term_brands = get_term_by( 'slug', $slug_brands, 'brands' );
-  $term_id_brands = $term_brands[0]->term_id;
+  $term_id_brands = $term_brands->term_id;
 ?>
-        <h1><?php echo $term_brands[0]->name; ?></h1>
-        <p><?php echo $term_brands[0]->description; ?></p>
+        <h1><?php echo $term_brands->name; ?></h1>
+        <p><?php echo $term_brands->description; ?></p>
 
         <?php
           $args=array(
@@ -29,26 +30,15 @@ get_header();
                 )
             )
           ); 
-        $terms = get_terms( 'product_categories', array(
-            'post_type' => 'products',
-            'parent'        => 0,             
-            'tax_query' => array(
-                 array(
-                'taxonomy'      => 'brands',    
-                'parent'        => $term_id_brands,
-                'terms'         => $term_brands,
-                'field'         => 'slug'
-                )
-              )
-            )
-            );
+         $categories=get_categories($args);
          
-            foreach($terms as $term) {
+            foreach($categories as $category) {
                 echo '<div class="product-cat">'; 
                 //$thumb_url = get_option('taxonomy_image_plugin');
-                echo '<a class="cat-title" href="http://jobesdev.com/brands/'.$term_brands[0]->slug.'/?product_categories='.$term[0]->slug.'">' . $term[0]->name.'</a>';
+                echo '<a class="cat-title" href="http://jobesdev.com/brands/'.$term_brands->slug.'/?cat='.$category->cat_ID.'">' . $category->name.'</a>';
                 echo '</div> <!--end product cat-->';
             }?>
+
 
       </div> <!--end of entry-->      
 
