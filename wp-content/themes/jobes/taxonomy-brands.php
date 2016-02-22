ce6d3cb0ce6494d68ff6e41c77905c66ccfce0e4
michelemccom <michele_martin@mccom.com>
2016-02-22 12:36:34 -0600

    cats

--- wp-content/themes/jobes/taxonomy-brands.php
+++ wp-content/themes/jobes/taxonomy-brands.php
@@ -48,8 +48,8 @@ get_header();
                $terms = array_map("unserialize", array_unique(array_map("serialize", $all_terms)));
 
                var_dump($terms);
-               foreach($terms as $term) {
-                   echo '<li><a href="'.$term['slug'][0].'">'.$term['name'][0].'</a></li>'; 
+               foreach($terms as list($a, $b) {
+                   echo '<li><a href="'.$$a.'">'.$b.'</a></li>'; 
                 
                     } ?>
         
