0022b42cd7186e704e80dffc5c3915b6a35e5b58
michelemccom <michele_martin@mccom.com>
2016-02-23 09:19:09 -0600

    brands page ;

--- wp-content/themes/jobes/page-brands.php
+++ wp-content/themes/jobes/page-brands.php
@@ -26,7 +26,7 @@ if( $terms ): ?>
 	
 		<li class="block">
 		 <?php if ( has_post_thumbnail() ) {
-			   $circle = get_field('brand_image', $term ) 	
+			   $circle = get_field('brand_image', $term );
 		    if (!empty($circle)) { ?>
 		      	<div class="round">
 					<a href="<?php the_permalink(); ?>"><img src="<?php echo $circle;?>">	</a>
