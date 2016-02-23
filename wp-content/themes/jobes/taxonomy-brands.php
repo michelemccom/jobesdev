75370b1e31df2dc5241a970e8f45d3917c0d865a
michelemccom <michele_martin@mccom.com>
2016-02-23 16:59:54 -0600

    if no cats, show products

--- wp-content/themes/jobes/taxonomy-brands.php
+++ wp-content/themes/jobes/taxonomy-brands.php
@@ -61,19 +61,60 @@ get_header();
               $all_termstwo[] = $term->slug;
 
             }
-          }
+          } 
+
           endwhile; endif;
           $terms = array_unique($all_terms);
-          $termstwo = array_unique($all_termstwo);
-          foreach (array_combine($terms, $termstwo) as $term => $termtwo) { ?>
-              <li class="block">
-               <div class="round">
-                   <a href="<?php echo get_option('home'); ?>/products/?brands=<?php echo $term_brands->slug;?>&product_categories=<?php echo $termtwo; ?>"><img src="<?php echo get_bloginfo('template_url')?>/images/<?php echo $term_brands->slug;?>_<?php echo $termtwo; ?>.jpg"> </a>
-                </div>
+          if ($terms) {
+            $termstwo = array_unique($all_termstwo);
+            foreach (array_combine($terms, $termstwo) as $term => $termtwo) { ?>
+                <li class="block">
+                 <div class="round">
+                     <a href="<?php echo get_option('home'); ?>/products/?brands=<?php echo $term_brands->slug;?>&product_categories=<?php echo $termtwo; ?>"><img src="<?php echo get_bloginfo('template_url')?>/images/<?php echo $term_brands->slug;?>_<?php echo $termtwo; ?>.jpg"> </a>
+                  </div>
 
-                 <h4><a href="<?php echo get_option('home'); ?>/products/?brands=<?php echo $term_brands->slug;?>&product_categories=<?php echo $termtwo; ?>"><?php echo $term;?> </a></h4>
-              </li>       
-          <?php } ?>
+                   <h4><a href="<?php echo get_option('home'); ?>/products/?brands=<?php echo $term_brands->slug;?>&product_categories=<?php echo $termtwo; ?>"><?php echo $term;?> </a></h4>
+                </li>       
+            <?php } 
+          } else {
+            $args=array(
+              'post_type' => 'products',
+              'parent'        => 0,             
+              'tax_query' => array(
+                   array(
+                  'taxonomy'      => 'brands',    
+                  'parent'        => $term_id_brands,
+                  'terms'         => $term_brands,
+                  'field'         => 'slug'
+                  )
+              )
+            ); 
+            $the_query = new WP_Query($args);
+            if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post(); ?>
+              <li class="block"> 
+                 <?php if ( has_post_thumbnail() ) {
+                $thumb_id = get_post_thumbnail_id();
+              $thumb_url = wp_get_attachment_image_src($thumb_id,'block', true);?>
+                    <?php if (!empty($thumb_url[0])) { ?>
+                    <div class="round">
+                  <a href="<?php the_permalink(); ?>"><img src="<?php echo $thumb_url[0];?>"> </a>
+                </div>
+                <?php }
+              } ?>
+
+                <?php if (!empty($thumb_url)) { ?>
+                <div class="round">
+              <img src="<?php echo $thumb_url[0];?>" width="<?php echo $thumb_url[1];?>" height="<?php echo $$thumb_url[2]; ?>">
+            </div>
+            <?php } ?>
+                <h4><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h4>
+
+                </li>
+
+
+             <?php endwhile; endif;wp_reset_postdata(); 
+
+          }?>
   
          
   </ul>
