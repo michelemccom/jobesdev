574f9bdd6f82cdd4cb7f65312ded378ebaedd9e5
Michele Martin <cmmartin0275@michelertinsmbp.attlocal.net>
2016-02-24 17:53:37 -0600

    404

--- wp-content/themes/jobes/404.php
+++ wp-content/themes/jobes/404.php
@@ -9,9 +9,9 @@ get_header();
 
 <div id="copy"> 
 
-    <h1>Not Found</h1>
-    <p>Sorry, but you are looking for something that isn't here.</p>
-    <?php get_search_form(); ?>
+    <h2>Whoops.</h2>
+    <h6>Looks like you're in the woods</h6>
+   
 
 </div>
 
