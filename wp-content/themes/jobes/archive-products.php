ba606061f183512058ef1f9e6e88a5f143d492a7
Michele Martin <cmmartin0275@michelertinsmbp.attlocal.net>
2016-02-24 15:12:54 -0600

    single redirect

--- wp-content/themes/jobes/archive-products.php
+++ wp-content/themes/jobes/archive-products.php
@@ -10,7 +10,6 @@ $parts = parse_url($url);
 parse_str($parts['query'], $query);
 $pbrand = $query['brands'];
 $pcat = $query['product_categories'];
-echo $pbrand.'-'.$pcat;
 ?>
 
 <div id="copy"> 
---
+++ wp-content/themes/jobes/single-brand_cat_desc.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Jobes
+ */
+
+get_header();
+
+header( 'Location: //' . $_SERVER['SERVER_NAME']) ;
+ get_footer(); ?>
