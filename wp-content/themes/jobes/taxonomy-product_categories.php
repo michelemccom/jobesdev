6906a3b8d1d4c6dd141fe7560b463c294b91526d
michelemccom <michele_martin@mccom.com>
2016-02-25 16:21:10 -0600

    brands

--- wp-content/themes/jobes/taxonomy-product_categories.php
+++ wp-content/themes/jobes/taxonomy-product_categories.php
@@ -11,6 +11,15 @@ get_header();
   $slug_pcats = get_query_var( 'term' );
   $term_pcats = get_term_by( 'slug', $slug_pcats, 'product_categories' );
   $term_id_pcats = $term_pcats->term_id;
+  function mergeArrays($terms, $termstwo, $termsID) {
+              $result = array();
+
+              foreach ( $terms as $key=>$name ) {
+                  $result[] = array( 'name' => $name, 'slug' => $termstwo[$key], 'pid' => $termsID[ $key ] );
+              }
+
+              return $result;
+          }
 ?>
 
 <div id="copy"> 
@@ -62,23 +71,23 @@ get_header();
           $terms = array_unique($all_terms);
           $termstwo = array_unique($all_termstwo);
           $termsID = array_unique($all_termsID);
-            foreach (array_combine($terms, $termstwo, $termsID) as $term => $termtwo =>$termID) { ?>
+
+
+            foreach ($result) { ?>
                 <li>
                     <div class="brand-header">
-      
-
-                     <?php $logo = get_field('brand_logo', $termID );
+                      <?php 
+                      $logo = get_field('brand_logo', $result[0]['pid'] );
                       
                         if (!empty($logo)){?>
                           <div class="logo-circle">
                             <span>
-                              <h1><?php echo $term; ?></h1>
-                              <img src="<?php echo $logo;?>" alt="<?php echo $term;?>">
+                              <h1><?php echo $result[0]['name']; ?></h1>
+                              <img src="<?php echo $logo;?>" alt="<?php echo $result[0]['name'];?>">
                             </span>
                           </div>
                         <?php } ?>
-                        <h4><a href="<?php echo get_option('home'); ?>/brands/<?php echo $termtwo; ?>"><?php echo $term;?> </a></h4>
-                      <?php } ?>
+                        <h4><a href="<?php echo get_option('home'); ?>/brands/<?php echo $result[0]['slug']; ?>"><?php echo $result[0]['name'];?> </a></h4>
                     </div>
                     
     
@@ -87,7 +96,7 @@ get_header();
                     'posts_per_page' => -1,
                     'order' => 'ASC',
                     'orderby' => 'date',
-                    'name' => $term.'-'.$term_pcats->slug
+                    'name' => $result[0]['name'].'-'.$term_pcats->slug
                     );
                     $the_query = new WP_Query($args);
                     if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post();
@@ -103,7 +112,7 @@ get_header();
                         'relation' => 'AND',
                          array(
                         'taxonomy'      => 'brands', 
-                        'terms'         => $termtwo,
+                        'terms'         => $result[0]['slug'],
                         'field'         => 'slug'
                         ),
                           array(
