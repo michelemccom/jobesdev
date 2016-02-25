06d39edc378349ce263c8d4241ebf6f8916b5793
michelemccom <michele_martin@mccom.com>
2016-02-25 14:04:01 -0600

    brands

--- wp-content/themes/jobes/taxonomy-product_categories.php
+++ wp-content/themes/jobes/taxonomy-product_categories.php
@@ -66,8 +66,8 @@ get_header();
                     <div class="brand-header">
                       <?php $brandID = get_the_terms( $post->ID, 'brands' ); 
 
-                      $logo = get_field('brand_logo', $term );
-                      var_dump($brandID);
+                      $logo = get_field('brand_logo', $term->term_id );
+                      
                         if (!empty($logo)){?>
                           <div class="logo-circle">
                             <span>
@@ -76,7 +76,7 @@ get_header();
                             </span>
                           </div>
                         <?php } ?>
-                        <h4><a href="<?php echo get_option('home'); ?>/products/brands/<?php echo $termtwo; ?>"><?php echo $term;?> </a></h4>
+                        <h4><a href="<?php echo get_option('home'); ?>/brands/<?php echo $termtwo; ?>"><?php echo $term;?> </a></h4>
                     </div>
                     
     
@@ -142,42 +142,7 @@ get_header();
                     <?php } ?>
                 </li>       
             <?php } 
-          } else {
-            $args=array(
-              'post_type' => 'products',
-              'parent'        => 0,             
-              'tax_query' => array(
-                   array(
-                  'taxonomy'      => 'brands',    
-                  'parent'        => $term_id_pcats,
-                  'terms'         => $term_pcats,
-                  'field'         => 'slug'
-                  )
-              )
-            ); 
-            $the_query = new WP_Query($args);
-            if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post(); ?>
-              <li class="block"> 
-                 <?php if ( has_post_thumbnail() ) {
-                $thumb_id = get_post_thumbnail_id();
-              $thumb_url = wp_get_attachment_image_src($thumb_id,'block', true);?>
-                    <?php if (!empty($thumb_url[0])) { ?>
-                    <div class="round">
-                  <a href="<?php the_permalink(); ?>"><img src="<?php echo $thumb_url[0];?>"> </a>
-                </div>
-                <?php }
-              } ?>
-
-            
-                <h4><?php the_title();?></h4>
-
-                </li>
-
-
-             <?php endwhile; endif;wp_reset_postdata(); 
-
-          }?>
-  
+          } ?>
         </span> 
   </ul>
 
