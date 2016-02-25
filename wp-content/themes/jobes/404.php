18388a5f7a515e69357653de30ca82ed52f8b231
michelemccom <michele_martin@mccom.com>
2016-02-25 09:09:13 -0600

    404 page

--- wp-content/themes/jobes/404.php
+++ wp-content/themes/jobes/404.php
@@ -9,11 +9,12 @@ get_header();
 
 <div id="copy"> 
 	<img src="<?php echo get_bloginfo('template_url')?>/images/404.png" />
-	<p class="red">404 (Page Not Found) Error</p>	
-    <h2>Whoops,</h2>
-    <h5>Looks like you're in the woods</h5>
-   
-   <p>Find what you're looking for at <a href="/">home</a>.</p>
+	<div class="right-copy">
+		<p class="red">404 (Page Not Found) Error</p>	
+	    <h2>Whoops,</h2>
+	    <h5>Looks like you're in the woods</h5>
+		<p>Find what you're looking for at <a href="/">home</a>.</p>
+	</div>
 
 </div>
 
