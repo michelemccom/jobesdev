8040ab6481d2ee466026cd4d8ad59ff0151c9f31
michelemccom <michele_martin@mccom.com>
2016-02-25 13:38:38 -0600

    brands

--- wp-content/themes/jobes/taxonomy-product_categories.php
+++ wp-content/themes/jobes/taxonomy-product_categories.php
@@ -82,7 +82,7 @@ get_header();
                         <?php } ?>
                         <h4><a href="<?php echo get_option('home'); ?>/products/brands/<?php echo $termtwo; ?>"><?php echo $term;?> </a></h4>
                     </div>
-                    <?php } ?>
+                    
                   </div>
                   <?php $args = array(
                     'post_type' => 'brand_cat_desc',
@@ -139,6 +139,7 @@ get_header();
 
                   <?php endwhile; ?>
                 </ul>
+                <?php } ?>
                   <?php endif;wp_reset_postdata(); ?>
                 </li>       
             <?php } 
