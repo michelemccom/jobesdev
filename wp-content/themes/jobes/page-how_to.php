2078b7ee27e3d82539ecb64ef3ada1f0d3510c4e
michelemccom <michele_martin@mccom.com>
2016-02-26 11:26:15 -0600

    double image

--- wp-content/themes/jobes/page-how_to.php
+++ wp-content/themes/jobes/page-how_to.php
@@ -28,14 +28,13 @@ get_header(); ?>
 	      	 <?php if ( has_post_thumbnail() ) {
 			    $thumb_id = get_post_thumbnail_id();
 				$thumb_url = wp_get_attachment_image_src($thumb_id,'block', true);?>
-			   
-
-		      	<?php if (!empty($thumb_url)) { ?>
-		      	<div class="round">
-					<a href="<?php the_permalink(); ?>"><img src="<?php echo $thumb_url[0];?>" width="<?php echo $thumb_url[1];?>" height="<?php echo $$thumb_url[2]; ?>">	</a>
-				</div>
-				<?php } ?>
-		      	<h4><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h4>
+			      	<?php if (!empty($thumb_url[0])) { ?>
+			      	<div class="round">
+						<a href="<?php the_permalink(); ?>"><img src="<?php echo $thumb_url[0];?>">	</a>
+					</div>
+					<?php }
+				} ?>      	
+				<h4><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h4>
 
 	      	</li>
 	    <?php endwhile; endif;wp_reset_postdata();?>
