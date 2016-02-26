eedfa87591b555be969a69811d0d6fcd922ba69c
michelemccom <michele_martin@mccom.com>
2016-02-26 12:38:02 -0600

    adding apostrophe

--- wp-content/themes/jobes/page-buy.php
+++ wp-content/themes/jobes/page-buy.php
@@ -14,16 +14,26 @@ $pbrand = $query['brand'];?>
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<h1 class="page-title"><?php the_title(); ?></h1>
 		<?php the_content(); ?>
-
+		<ul class="section">
 		<?php 
 	    $args = array(
-	        'post_type' => 'how_to',
+	        'post_type' => 'where',
 	        'posts_per_page' => -1,
 	        'order' => 'ASC',
-	        'orderby' => 'date'
+	        'orderby' => 'date',
+	         'tax_query' => array(
+           
+	             array(
+	            'taxonomy'      => 'brands', 
+	            'terms'         => $pbrand,
+	            'field'         => 'slug'
+	            	)
+	         )
 	        );
 	    $the_query = new WP_Query($args);
 	    if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post();?>
+	    <li><?php the_title(); ?></li>
 	    <?php endwhile; endif;wp_reset_postdata();?>
+	</ul>
 	<?php endwhile; endif; ?>   
 <?php get_footer(); ?>
