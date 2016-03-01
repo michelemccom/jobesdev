fb7a678e64c949f216836c7afd975e3f11da4fa3
michelemccom <michele_martin@mccom.com>
2016-03-01 14:49:01 -0600

    where to buy

--- wp-content/themes/jobes/page-buy.php
+++ wp-content/themes/jobes/page-buy.php
@@ -14,7 +14,7 @@ $pbrand = $query['brand'];?>
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<h1 class="page-title"><?php the_title(); ?></h1>
 		<?php the_content(); ?>
-	<ul class="blocks section">
+	<ul class="blocks where section">
 		<?php 
 	    $args = array(
 	        'post_type' => 'where',
