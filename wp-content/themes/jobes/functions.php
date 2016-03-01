1993cb2e49b8aaa09d60962af265aa3c9b41a6ef
michelemccom <michele_martin@mccom.com>
2016-03-01 11:18:42 -0600

    changing where img size

--- wp-content/themes/jobes/functions.php
+++ wp-content/themes/jobes/functions.php
@@ -81,7 +81,8 @@ add_action( 'admin_menu', 'remove_post_boxes' );
 add_theme_support( 'post-thumbnails' ); 
 
 if ( function_exists( 'add_image_size' ) ) { 
-	add_image_size( 'block', 240, 240, true );
+	add_image_size( 'block', 280, 280, true );
+  add_image_size( 'where', 220, 220, true );
   add_image_size( 'logo', 280, 90 );
 }
 
--- wp-content/themes/jobes/page-buy.php
+++ wp-content/themes/jobes/page-buy.php
@@ -35,7 +35,7 @@ $pbrand = $query['brand'];?>
 	      <li class="block"> 
 	      	 <?php if ( has_post_thumbnail() ) {
 			    $thumb_id = get_post_thumbnail_id();
-				$thumb_url = wp_get_attachment_image_src($thumb_id,'block');?>
+				$thumb_url = wp_get_attachment_image_src($thumb_id,'where');?>
 			      	<?php if (!empty($thumb_url)) { ?>
 			      	<div class="round">
 						<a href="<?php the_permalink(); ?>"><img src="<?php echo $thumb_url[0];?>">	</a>
