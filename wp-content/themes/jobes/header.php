a5e512fbd43691b138935269027560dd118da96b
michelemccom <michele_martin@mccom.com>
2016-03-03 15:28:48 -0600

    mobile 768

--- wp-content/themes/jobes/header.php
+++ wp-content/themes/jobes/header.php
@@ -35,17 +35,19 @@ filter: none;
 <script type="text/javascript">
     jQuery( document ).ready(function() {
         jQuery('.menu-icon').click( function(event){
-        event.preventDefault();
-        if (jQuery('.mobile-nav ul').hasClass("open") ) {
-            jQuery('.mobile-nav ul').removeClass("open");
-            jQuery('.mobile-nav ul').addClass("closed");
-        } else {    
-            jQuery('.mobile-nav ul').removeClass("closed");
-            jQuery('.mobile-nav ul').addClass("open");
-        }
-        return false;
+            event.preventDefault();
+            if (jQuery('.mobile-nav ul').hasClass("open") ) {
+                jQuery('.mobile-nav ul').removeClass("open");
+                jQuery('.mobile-nav ul').addClass("closed");
+            } else {    
+                jQuery('.mobile-nav ul').removeClass("closed");
+                jQuery('.mobile-nav ul').addClass("open");
+            }
+            return false;
+        });
     });
-    )};
+
+ 
 
 
 </script>
